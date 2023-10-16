32d22901a662646ee3a7e3ac0bb58a283f07b87b
gykunju <alvingikunju@gmail.com>
2023-10-16 14:59:35 +0300

    while loop created

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=2,b=7;
+    while(a < b)
+    {
+        printf("adding 1 to %d\n",a);
+        a++;
+    }
+    printf("a and b are equal now");
+    return 0;
+}
